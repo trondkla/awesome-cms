e9523caed412561dda5d0620ab5959192363df11
Trond Klakken <trond@klakken.com>
2015-02-05 23:17:21 +0100

    First version! #1337

---
+++ cms.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+
+var CSM = {};
+
+CMS.version = "1337.0";
+
+CMS.getPage = function(id) {
+   fs.readFile("page/" + id, "utf8", function (err, data) {
+      if(err) {
+         return "404 - Page not found";
+      }
+      return data;
+   }
+};
+
+module.exports = CMS
